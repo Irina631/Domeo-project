c193a134e9379c5d1e3a4004e5df1f42e67538b8
irina <example@gmail.com>
2021-08-02 16:07:18 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const DATA = [
+    {
+        question: 'Какая у вас специализация?',
+        answers: [
+            {
+                
+            }
+        ]
+    }
+]
+
+const quiz = document.getElementById('quiz');
+const questions = document.getElementById('questions');
+const btnPrev = document.getElementById('btn-prev');
+const btnNext = document.getElementById('btn-next');
